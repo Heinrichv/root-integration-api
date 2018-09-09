ebcfae9bd27df6e1efef9fc9ad5b9eeb43329ba7
Heinrich Venter <heini141@outlook.com>
2018-09-09 16:37:11 +0200

    initial commit

---
+++ lib/server.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import { useExpressServer } from 'routing-controllers';
+import expressApp from './startup';
+
+const port = 4040;
+
+useExpressServer(expressApp, {
+  routePrefix: '/api',
+  cors: {
+    // options from cors documentation
+  },
+  controllers: [
+    __dirname + '/Controllers/*.ts]'
+  ],
+  middlewares: [
+
+  ]
+});
+
+expressApp.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
---
+++ lib/startup.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import { Application } from 'express';
+
+class Startup {
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  public app: Application;
+
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+}
+
+export default new Startup().app;
